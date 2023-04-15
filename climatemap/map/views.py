630e16264a5e320627d6f4cb12dab0713059be54
Param <param23403@gmail.com>
2023-04-15 12:16:05 -0400

    calculator embedded

--- climatemap/map/views.py
+++ climatemap/map/views.py
@@ -1,23 +1,30 @@
 from django.shortcuts import render
 import geopandas
+import requests
 
 
 def index(request):
     return render(request, "map.html")
 
+
 def calculator(request):
-    return render(request,'calculator.html')
+    return render(request, 'calculator.html')
+
+
+def calculator(request):
+
+    return render(request, "calculator.html")
+
 
 def climate_map(request):
     return render(request, "climate_map.html")
 
-def update_map(request):
-    world = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
 
-    
+def update_map(request):
+    world = geopandas.read_file(
+        geopandas.datasets.get_path('naturalearth_lowres'))
 
     climate_map = world.explore()
     climate_map.save("map/templates/climate_map.html")
 
     return render(request, "map.html")
-
