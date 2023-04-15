2ab26735f51004b36647230d1eed8de03d4b256c
Param <param23403@gmail.com>
2023-04-15 11:07:25 -0400

    conflict fixed

--- climatemap/map/urls.py
+++ climatemap/map/urls.py
@@ -4,11 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-<<<<<<< HEAD
     path('climate_map', views.climate_map, name='climate_map'),
     path('update_map', views.update_map, name='update_map'),
-
-=======
     path('calculator',views.calculator, name='calculator')
->>>>>>> 7a37ee51b873260b296c836d5e32f1bfe47a75e7
 ]
