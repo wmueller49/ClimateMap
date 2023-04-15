f85b48d30a8c850edd15026aaeea54457846d1d8
Param <param23403@gmail.com>
2023-04-15 14:19:50 -0400

    gpt3pushed

--- climatemap/map/views.py
+++ climatemap/map/views.py
@@ -1,20 +1,32 @@
 from django.shortcuts import render
 import geopandas
 import requests
+from django.shortcuts import render, redirect
+import openai
+from .secret_key import API_KEY
+
+openai.api_key = API_KEY
 
 
 def index(request):
     return render(request, "map.html")
 
-
 def calculator(request):
-    return render(request, 'calculator.html')
-
-
-def calculator(request):
-
-    return render(request, "calculator.html")
-
+    try:
+        if request.method == 'POST':
+            prompt = request.POST.get('prompt')
+            response = openai.Completion.create(model="text-davinci-003", prompt=prompt, temperature=1, max_tokens=1000)
+            formatted_response = response['choices'][0]['text']
+            context = {
+                'formatted_response': formatted_response,
+                'prompt': prompt
+            }
+            return render(request, "calculator.html",context)
+        else:
+            return render(request, "calculator.html")
+    except:
+        # this will redirect to the same page after any error is caught
+            return render(request, "calculator.html")
 
 def climate_map(request):
     return render(request, "climate_map.html")
